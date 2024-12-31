bd4b921ba22d6d92d0f60c1d124164e89e9dd74a
AymanBj99 <ayman.bouajaji66@gmail.com>
2024-12-31 20:07:57 +0100

    charger et visualiser dataset

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import pandas as pd
 import missingno as msno
 #get the wine dataset from sklearn and take a look at the description provided
 from sklearn import datasets
+
+#charger et visualiser dataset
+wine = datasets.load_wine()
+print(wine.DESCR)
+
