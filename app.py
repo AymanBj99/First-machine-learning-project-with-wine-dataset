6d09648300d8d73f97fd3ca951def4fa76c76122
AymanBj99 <ayman.bouajaji66@gmail.com>
2025-01-01 02:22:02 +0100

    entrainer le modele

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
 # Charger le dataset wine
@@ -49,3 +50,9 @@ X_scaled = scaler.fit_transform(X)  # Normalisation des caractéristiques
 
 # Séparer les données en ensembles d'entraînement (70%) et de test (30%)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
+
+# Créer un modèle KNN avec 5 voisins
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Entraîner le modèle
+knn.fit(X_train, y_train)
